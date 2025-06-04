2d6aa00b7910fed28f0ef5d7ec6814d07ac7f696
camiloRondonDev <camiloanla@gmail.com>
2025-06-03 22:56:39 -0500

    cambio estructura de home con php

---
+++ core/app.php
@@ -0,0 +1,6 @@
+<?php
+class App {
+    public function __construct() {
+        echo "¡App cargada correctamente!";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
-require_once '../app/controllers/HomeController.php';
+require_once '../core/App.php';
+require_once '../core/Controller.php';
+require_once '../config/config.php';
 
-$controller = new HomeController();
-$controller->index();
+$app = new App(); // Se encarga de todo automáticamente
 ?>
