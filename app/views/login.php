f19c99ff46d7bd466a37ffb151de3dbae1954625
Sebastian Salon <165338635+SebastianSalon@users.noreply.github.com>
2025-06-27 18:30:00 -0500

    arreglo login

--- app/views/login.php
+++ app/views/login.php
@@ -1,20 +1,28 @@
 <?php require_once 'header.php'; ?>
-<link rel="stylesheet" href="/amt/public/css/login.css">
+<link rel="stylesheet" href="/amt/public/css/login.css?v=3">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
-<main>
-<div class="login-container">
+<main class="login-pagina">
+  <div class="login-fondo">
+    <div class="login-container">
     <form id="formLogin" class="login-form" action="#" method="POST">
-        <h2>Iniciar sesión</h2>
-        <div class="input-group">
-            <label for="username">Usuario</label>
-            <input type="text" id="username" name="username" placeholder="Introduce tu usuario" required>
-        </div>
-        <div class="input-group">
-            <label for="password">Contraseña</label>
-            <input type="password" id="password" name="password" placeholder="Introduce tu contraseña" required>
-        </div>
-        <button type="submit" class="btn-login">Iniciar sesión</button>
-        
+   <div class="login-header">
+  <img src="/amt/public/img/VECTORIZADO VERTICAL.png" alt="Logo" class="login-logo">
+  <h2 class="login-titulo">Iniciar sesión</h2>
+</div>
+
+<div class="input-group">
+  <i class="fas fa-user"></i>
+  <input type="text" id="username" name="username" placeholder="Usuario" required>
+</div>
+
+<div class="input-group">
+  <i class="fas fa-lock"></i>
+  <input type="password" id="password" name="password" placeholder="Contraseña" required>
+</div>
+
+        <button type="submit" class="btn-fancy-login">Iniciar sesión</button>
+                
         <div class="login-options">
             <a href="create_user.php" class="btn-create-account">Crear cuenta nueva</a>
             <a href="#" class="btn-recover-password">¿Olvidaste tu contraseña?</a>
@@ -23,6 +31,7 @@
 
 </div>
 
+  </div>
 </main>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
